8ff021c8484ef046a912c92ef2b0eb9203387db1
anupmehta702 <anupmehta702@gmail.com>
2021-04-23 20:15:18 +0530

    Update TransactionFeeVer3.py

--- TransactionFeeVer3.py
+++ TransactionFeeVer3.py
@@ -55,16 +55,48 @@ def MyJoinTransform(glueContext, dfc) -> DynamicFrameCollection:
     dfcCustom = dfc.select(list(dfc.keys())[0]).toDF()
     dfcCustom.show();
     
+    
+    
     # dd/mm/YY H:M:S
     now = datetime.now().strftime("%d%m%Y%H:%M:%S")
     
     oneFileTransform = dfcCustom.repartition(1)
-    oneFileTransform.write.json("s3://billing-account-glue-output/transaction/joinFeed/"+now+"/")
+    oneFileTransform.write.csv("s3://billing-account-glue-output/transaction/joinFeed/"+now+"/")
     
     
     dailyCustomerAccountBalance = DynamicFrame.fromDF(dfcCustom, glueContext, "dailyCustomerAccountBalance")
-    return (DynamicFrameCollection({"CustomTransform0": dailyCustomerAccountBalance}, glueContext))
+    return (DynamicFrameCollection({"CustomTransform2": dailyCustomerAccountBalance}, glueContext))
      
+def MyFileBeautificationTransform(glueContext, dfc) -> DynamicFrameCollection:
+    
+    from pyspark.sql import SparkSession
+    from pyspark.sql.functions import when,col 
+    from datetime import date
+    from datetime import datetime
+    
+    dfcCustom = dfc.select(list(dfc.keys())[0]).toDF()
+    
+    
+    #Create DF with headers
+    spark = SparkSession.builder.appName('TransactionFeed').getOrCreate()
+    simpleData = [("accountId","sortKey","chargeid","transactionid","charge_amount","snapshotSortkey","snapshotTransactionId","status")]
+    columns= ["accountId","sortKey","chargeid","transactionid","charge_amount","snapshotSortkey","snapshotTransactionId","status"]
+    df = spark.createDataFrame(data = simpleData, schema = columns)
+    
+    
+    #Create union
+    unionDF = df.union(dfcCustom)
+    unionDF.show()
+    
+    # Write to file with dd/mm/YY H:M:S
+    now = datetime.now().strftime("%d%m%Y%H:%M:%S")
+    oneFileTransform = unionDF.repartition(1)
+    oneFileTransform.write.csv("s3://billing-account-glue-output/transaction/joinFeed/feedWithHeader/"+now+"/")
+    
+    
+    dailyTransactionFeedWithHeader = DynamicFrame.fromDF(unionDF, glueContext, "dailyTransactionFeedWithHeader")
+    
+    return (DynamicFrameCollection({"CustomTransform3": dailyTransactionFeedWithHeader}, glueContext))
 
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -79,46 +111,61 @@ job.init(args['JOB_NAME'], args)
 ## @return: DataSource0
 ## @inputs: []
 DataSource0 = glueContext.create_dynamic_frame.from_catalog(database = "billingaccountdb", table_name = "transaction_transaction", transformation_ctx = "DataSource0")
+## @type: ApplyMapping
+## @args: [mappings = [("accountid", "string", "accountid", "string"), ("sortkey", "string", "sortkey", "string"), ("chargeid", "string", "chargeid", "string"), ("transactionid", "string", "transactionid", "string"), ("charge_amount", "string", "charge_amount", "string")], transformation_ctx = "Transform4"]
+## @return: Transform4
+## @inputs: [frame = DataSource0]
+Transform4 = ApplyMapping.apply(frame = DataSource0, mappings = [("accountid", "string", "accountid", "string"), ("sortkey", "string", "sortkey", "string"), ("chargeid", "string", "chargeid", "string"), ("transactionid", "string", "transactionid", "string"), ("charge_amount", "string", "charge_amount", "string")], transformation_ctx = "Transform4")
 ## @type: CustomCode
-## @args: [dynamicFrameConstruction = DynamicFrameCollection({"DataSource0": DataSource0}, glueContext), className = MyTransform, transformation_ctx = "Transform0"]
+## @args: [dynamicFrameConstruction = DynamicFrameCollection({"Transform4": Transform4}, glueContext), className = MyTransform, transformation_ctx = "Transform0"]
 ## @return: Transform0
-## @inputs: [dfc = DataSource0]
-Transform0 = MyTransform(glueContext, DynamicFrameCollection({"DataSource0": DataSource0}, glueContext))
+## @inputs: [dfc = Transform4]
+Transform0 = MyTransform(glueContext, DynamicFrameCollection({"Transform4": Transform4}, glueContext))
 ## @type: SelectFromCollection
-## @args: [key = list(Transform0.keys())[0], transformation_ctx = "Transform6"]
-## @return: Transform6
+## @args: [key = list(Transform0.keys())[0], transformation_ctx = "Transform9"]
+## @return: Transform9
 ## @inputs: [dfc = Transform0]
-Transform6 = SelectFromCollection.apply(dfc = Transform0, key = list(Transform0.keys())[0], transformation_ctx = "Transform6")
+Transform9 = SelectFromCollection.apply(dfc = Transform0, key = list(Transform0.keys())[0], transformation_ctx = "Transform9")
 ## @type: DataSource
 ## @args: [database = "billingaccountdb", table_name = "ver3_customeraccountbalance", transformation_ctx = "DataSource1"]
 ## @return: DataSource1
 ## @inputs: []
 DataSource1 = glueContext.create_dynamic_frame.from_catalog(database = "billingaccountdb", table_name = "ver3_customeraccountbalance", transformation_ctx = "DataSource1")
 ## @type: ApplyMapping
-## @args: [mappings = [("sortkey", "string", "snapshotSortkey", "string"), ("transactionid", "string", "(right) transactionid", "string"), ("ppeaccountid", "string", "(right) ppeaccountid", "string"), ("status", "string", "(right) status", "string")], transformation_ctx = "Transform5"]
-## @return: Transform5
+## @args: [mappings = [("sortkey", "string", "snapshotSortkey", "string"), ("transactionid", "string", "(right) transactionid", "string"), ("status", "string", "(right) status", "string")], transformation_ctx = "Transform8"]
+## @return: Transform8
 ## @inputs: [frame = DataSource1]
-Transform5 = ApplyMapping.apply(frame = DataSource1, mappings = [("sortkey", "string", "snapshotSortkey", "string"), ("transactionid", "string", "(right) transactionid", "string"), ("ppeaccountid", "string", "(right) ppeaccountid", "string"), ("status", "string", "(right) status", "string")], transformation_ctx = "Transform5")
+Transform8 = ApplyMapping.apply(frame = DataSource1, mappings = [("sortkey", "string", "snapshotSortkey", "string"), ("transactionid", "string", "(right) transactionid", "string"), ("status", "string", "(right) status", "string")], transformation_ctx = "Transform8")
 ## @type: CustomCode
-## @args: [dynamicFrameConstruction = DynamicFrameCollection({"Transform5": Transform5}, glueContext), className = MySnapshotTransform, transformation_ctx = "Transform3"]
-## @return: Transform3
+## @args: [dynamicFrameConstruction = DynamicFrameCollection({"Transform8": Transform8}, glueContext), className = MySnapshotTransform, transformation_ctx = "Transform5"]
+## @return: Transform5
+## @inputs: [dfc = Transform8]
+Transform5 = MySnapshotTransform(glueContext, DynamicFrameCollection({"Transform8": Transform8}, glueContext))
+## @type: SelectFromCollection
+## @args: [key = list(Transform5.keys())[0], transformation_ctx = "Transform2"]
+## @return: Transform2
 ## @inputs: [dfc = Transform5]
-Transform3 = MySnapshotTransform(glueContext, DynamicFrameCollection({"Transform5": Transform5}, glueContext))
+Transform2 = SelectFromCollection.apply(dfc = Transform5, key = list(Transform5.keys())[0], transformation_ctx = "Transform2")
+## @type: Join
+## @args: [columnConditions = ["="], joinType = right, keys2 = ["(right) transactionid"], keys1 = ["transactionid"], transformation_ctx = "Transform7"]
+## @return: Transform7
+## @inputs: [frame1 = Transform9, frame2 = Transform2]
+Transform9DF = Transform9.toDF()
+Transform2DF = Transform2.toDF()
+Transform7 = DynamicFrame.fromDF(Transform9DF.join(Transform2DF, (Transform9DF['transactionid'] == Transform2DF['(right) transactionid']), "right"), glueContext, "Transform7")
+## @type: CustomCode
+## @args: [dynamicFrameConstruction = DynamicFrameCollection({"Transform7": Transform7}, glueContext), className = MyJoinTransform, transformation_ctx = "Transform3"]
+## @return: Transform3
+## @inputs: [dfc = Transform7]
+Transform3 = MyJoinTransform(glueContext, DynamicFrameCollection({"Transform7": Transform7}, glueContext))
 ## @type: SelectFromCollection
 ## @args: [key = list(Transform3.keys())[0], transformation_ctx = "Transform1"]
 ## @return: Transform1
 ## @inputs: [dfc = Transform3]
 Transform1 = SelectFromCollection.apply(dfc = Transform3, key = list(Transform3.keys())[0], transformation_ctx = "Transform1")
-## @type: Join
-## @args: [columnConditions = ["="], joinType = right, keys2 = ["(right) transactionid"], keys1 = ["transactionid"], transformation_ctx = "Transform4"]
-## @return: Transform4
-## @inputs: [frame1 = Transform6, frame2 = Transform1]
-Transform6DF = Transform6.toDF()
-Transform1DF = Transform1.toDF()
-Transform4 = DynamicFrame.fromDF(Transform6DF.join(Transform1DF, (Transform6DF['transactionid'] == Transform1DF['(right) transactionid']), "right"), glueContext, "Transform4")
 ## @type: CustomCode
-## @args: [dynamicFrameConstruction = DynamicFrameCollection({"Transform4": Transform4}, glueContext), className = MyJoinTransform, transformation_ctx = "Transform2"]
-## @return: Transform2
-## @inputs: [dfc = Transform4]
-Transform2 = MyJoinTransform(glueContext, DynamicFrameCollection({"Transform4": Transform4}, glueContext))
+## @args: [dynamicFrameConstruction = DynamicFrameCollection({"Transform1": Transform1}, glueContext), className = MyFileBeautificationTransform, transformation_ctx = "Transform6"]
+## @return: Transform6
+## @inputs: [dfc = Transform1]
+Transform6 = MyFileBeautificationTransform(glueContext, DynamicFrameCollection({"Transform1": Transform1}, glueContext))
 job.commit()
